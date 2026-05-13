6bdfc96dbc34e9ad9d4316d6b58bd805776d7759
Jacob Morrison <jacob.ryan.morrison@gmail.com>
2026-05-13 09:57:14 -0500

    remove obsidian and SyncColorscheme logic

--- init.lua
+++ init.lua
@@ -1,88 +1,16 @@
---[[init
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
--- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- ============================================================
+-- SECTION 1: FOUNDATION
+-- Core Neovim settings, leaders, options, basic keymaps, basic autocmds
+-- ============================================================
+-- [[ Vim global settings ]]
+-- See `:help vim.g`
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-- For more options, you can see `:help option-list`
 
 vim.o.number = true
 vim.o.relativenumber = true
@@ -90,12 +18,8 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 vim.o.foldenable = false
-
--- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
-
--- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
+-- vim.o.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
@@ -103,13 +27,10 @@ vim.o.showmode = false
 --  See `:help 'clipboard'`
 vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
--- Enable break indent
 vim.o.breakindent = true
 
--- Enable undo/redo changes even after closing and reopening a file
 vim.o.undofile = true
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
@@ -138,10 +59,6 @@ vim.o.list = true
 -- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.o.fillchars = 'eob: '
 
--- Conceal level modified for Obsidian features to work
---@source https://github.com/epwalsh/obsidian.nvim#concealing-characters
-vim.opt.conceallevel = 2
-
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
 
@@ -157,7 +74,7 @@ vim.o.scrolloff = 10
 vim.o.confirm = true
 
 -- [[ Basic Keymaps ]]
---  See `:help vim.keymap.set()`
+-- See `:help vim.keymap.set()`
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
@@ -963,47 +880,6 @@ require('lazy').setup({
     },
   },
 })
--- Colorschemes are managed in lua/custom/plugins/colorscheme.lua
--- Run :ThemeSync to re-apply after switching your Ghostty theme
--- [[ Ghostty theme sync ]]
--- local function apply_ghostty_theme()
---   local ghostty_config = vim.fn.expand '~/Library/Application Support/com.mitchellh.ghostty/config*'
---   local ok, lines = pcall(vim.fn.readfile, ghostty_config)
---   if not ok then
---     print 'Error reading ghostty config. Setting fallback theme.'
---     vim.cmd 'colorscheme 0x96f'
---     return
---   end
---
---   local theme_map = {
---     ['0x96f'] = '0x96f',
---     ['Rose Pine'] = 'rose-pine',
---     ['Ayu Mirage'] = 'neovim-ayu',
---     ['Nvim Dark'] = ,
---   }
---
---   for _, line in pairs(lines) do
---     local theme = line:match '^%s*theme%s*=%s*(.-)%s*$'
---     if theme then
---       local nvim_theme = theme_map[theme]
---       if nvim_theme then
---         vim.cmd('colorscheme ' .. nvim_theme)
---       else
---         vim.notify('ThemeSync: no Neovim mapping for Ghostty theme "' .. theme .. '"', vim.log.levels.WARN)
---         vim.cmd 'colorscheme 0x96f'
---       end
---       return
---     end
---   end
---
---   vim.cmd 'colorscheme 0x96f'
--- end
---
--- apply_ghostty_theme()
---
--- vim.api.nvim_create_user_command('ThemeSync', apply_ghostty_theme, {
---   desc = 'Re-sync Neovim colorscheme from Ghostty config',
--- })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -2,6 +2,19 @@
 ---@type LazySpec
 return {
   {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      -- vim.g.everforest_background = 'hard'
+      vim.g.everforest_better_performance = true
+      vim.g.everforest_transparent_background = 2
+      vim.cmd.colorscheme 'everforest'
+    end,
+  },
+  {
     'Shatur/neovim-ayu',
     lazy = true,
   },
